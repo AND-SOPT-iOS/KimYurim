7d62a2a4c4ec39a2a55bfa148c4d6d05a669fce3
Yurim Kim <yurim830@gmail.com>
2024-10-15 20:37:43 +0900

    [Add] #6 - AppDetail View, VC 생성

---
+++ 35-seminar/Presentation/Week2/AppDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AppDetailView.swift
+//  35-seminar
+//
+//  Created by 김유림 on 10/15/24.
+//
+
+import UIKit
+
+class AppDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
