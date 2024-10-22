b218f391e6835980e5e82a78f5128f17b764618b
Yurim Kim <yurim830@gmail.com>
2024-10-22 18:28:44 +0900

    [Add] #6 - UILabel Extension

--- 35-seminar/Presentation/Week2/Extensions/UILabel+Extension.swift
+++ 35-seminar/Presentation/Week2/Extensions/UILabel+Extension.swift
@@ -5,4 +5,14 @@
 //  Created by 김유림 on 10/15/24.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func configureLabel(alignment: NSTextAlignment = .left, color: UIColor = .label, size: CGFloat, weight: UIFont.Weight, text: String? = nil, numberOfLines: Int = 1) {
+        self.textAlignment = alignment
+        self.textColor = color
+        self.font = .systemFont(ofSize: size, weight: weight)
+        self.text = text
+        self.numberOfLines = numberOfLines
+    }
+}
