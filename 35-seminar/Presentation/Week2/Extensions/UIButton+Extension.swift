38591f7eca02a977d71507d02a6eb6a158437ed9
Yurim Kim <yurim830@gmail.com>
2024-10-24 18:47:47 +0900

    [Add, Feat] #6 - (UIButton+Extension) Configure 함수 생성

---
+++ 35-seminar/Presentation/Week2/Extensions/UIButton+Extension.swift
@@ -0,0 +1,59 @@
+//
+//  UIButton+Extension.swift
+//  35-seminar
+//
+//  Created by 김유림 on 10/24/24.
+//
+
+import UIKit
+
+enum ConfigurationType {
+    case plain
+    case filled
+}
+
+extension UIButton {
+    func configureButton(configType: ConfigurationType = .plain,
+                         title: String? = nil,
+                         fontSize: CGFloat = 17,
+                         fontWeight: UIFont.Weight = .regular,
+                         image: UIImage? = nil,
+                         symbolWeight: UIImage.SymbolWeight = .unspecified,
+                         cornerStyle: UIButton.Configuration.CornerStyle? = nil,
+                         foregroundColor: UIColor = .tintColor,
+                         backgroundColor: UIColor = .clear,
+                         for state: UIControl.State = .normal) {
+        
+        var config = {
+            switch configType {
+            case .plain:
+                return UIButton.Configuration.plain()
+            case .filled:
+                return UIButton.Configuration.filled()
+            }
+        }()
+        
+        print(config)
+        
+        if let title = title {
+            let attributes: [NSAttributedString.Key: Any] = [.font : UIFont.systemFont(ofSize: fontSize, weight: fontWeight)]
+            let attributedTitle = NSAttributedString(string: title, attributes: attributes)
+            self.setAttributedTitle(attributedTitle, for: state)
+        }
+        
+        if let image = image {
+            let symbolConfig = UIImage.SymbolConfiguration(weight: symbolWeight)
+            image.withConfiguration(symbolConfig)
+            config.image = image
+        }
+        
+        if let cornerStyle = cornerStyle {
+            config.cornerStyle = cornerStyle
+        }
+        
+        config.baseForegroundColor = foregroundColor
+        config.baseBackgroundColor = backgroundColor
+        
+        self.configuration = config
+    }
+}
