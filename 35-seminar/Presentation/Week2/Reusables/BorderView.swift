8675705294d014fb036207832f72362dac96c59b
Yurim Kim <yurim830@gmail.com>
2024-10-24 20:10:47 +0900

    [Feat] #6 - 색상만 설정

--- 35-seminar/Presentation/Week2/Reusables/BorderView.swift
+++ 35-seminar/Presentation/Week2/Reusables/BorderView.swift
@@ -10,38 +10,12 @@ import SnapKit
 
 class BorderView: UIView {
     
-    // MARK: - Properties
-    let grayLine = UIView()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setUI()
-        setHierarchy()
-        setConstraints()
+        self.backgroundColor = .systemGray5
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setUI() {
-        self.backgroundColor = .none
-        grayLine.backgroundColor = .systemGray5
-    }
-    
-    private func setHierarchy() {
-        self.addSubview(grayLine)
-    }
-    
-    private func setConstraints() {
-        self.snp.makeConstraints {
-            $0.height.equalTo(0.5)
-        }
-        
-        grayLine.snp.makeConstraints {
-            $0.height.equalTo(0.5)
-            $0.horizontalEdges.equalToSuperview().inset(20)
-            $0.verticalEdges.equalToSuperview()
-        }
     }
 }
