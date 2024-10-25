72187880d1055c23c70e5ba412367a83f62f0764
Yurim Kim <yurim830@gmail.com>
2024-10-25 22:13:26 +0900

    줄맞춤

--- 35-seminar/Presentation/Week2/View/AllFeedbackViewController.swift
+++ 35-seminar/Presentation/Week2/View/AllFeedbackViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AllFeedbackViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
--- 35-seminar/Presentation/Week2/View/VersionRecordViewController.swift
+++ 35-seminar/Presentation/Week2/View/VersionRecordViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class VersionRecordViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
