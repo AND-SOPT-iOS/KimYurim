dd8f4e0c08d9c03e3abd1d623432e2784084cd1c
Yurim <yurim830@gmail.com>
2024-10-25 15:30:50 +0900

    [Add] #6 - (VersionRecordVC) 생성

---
+++ 35-seminar/Presentation/Week2/View/VersionRecordViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VersionRecordViewController.swift
+//  35-seminar
+//
+//  Created by 김유림 on 10/25/24.
+//
+
+import UIKit
+
+class VersionRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
