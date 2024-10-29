d128f24746b1b479643ea77aff2c791d5bc68aac
Yurim Kim <yurim830@gmail.com>
2024-10-30 04:39:41 +0900

    [Feat] #8 - (ChartTableViewCell) 디자인 완성

--- 35-seminar/Presentation/Appstore/Chart/ChartTableViewCell.swift
+++ 35-seminar/Presentation/Appstore/Chart/ChartTableViewCell.swift
@@ -48,25 +48,27 @@ class ChartTableViewCell: UITableViewCell {
     private func setUI() {
         labelHorizontalStackView.axis = .horizontal
         labelHorizontalStackView.alignment = .top
-        labelHorizontalStackView.spacing = 10
+        labelHorizontalStackView.spacing = 16
         
         titleStackView.axis = .vertical
         titleStackView.alignment = .leading
-        titleStackView.spacing = 2
+        titleStackView.spacing = 4
         
+        iconImageView.backgroundColor = .systemGray5
         iconImageView.contentMode = .scaleAspectFit
         iconImageView.clipsToBounds = true
-        iconImageView.layer.cornerRadius = 10
+        iconImageView.layer.cornerRadius = 14
         iconImageView.layer.borderColor = UIColor.systemGray5.cgColor
         iconImageView.layer.borderWidth = 1
         
-        rankingLabel.configureLabel(size: 17, weight: .bold)
+        rankingLabel.configureLabel(size: 17, weight: .semibold)
         rankingLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         
         titleLabel.configureLabel(color: .label, size: 17, weight: .regular, numberOfLines: 2)
         titleLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
         
         subtitleLabel.configureLabel(color: .secondaryLabel, size: 14, weight: .regular, numberOfLines: 2)
+        subtitleLabel.setContentHuggingPriority(.defaultLow, for: .vertical)
     }
     
     private func setHierarchy() {
@@ -86,14 +88,18 @@ class ChartTableViewCell: UITableViewCell {
     private func setConstraints() {
         iconImageView.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(20)
-            $0.verticalEdges.equalToSuperview().inset(10)
-            $0.size.equalTo(60)
+            $0.verticalEdges.equalToSuperview().inset(12)
+            $0.size.equalTo(64)
         }
         
         labelHorizontalStackView.snp.makeConstraints {
-            $0.leading.equalTo(iconImageView.snp.trailing).offset(10)
-            $0.trailing.equalTo(downloadButton.snp.leading).offset(-10)
+            $0.leading.equalTo(iconImageView.snp.trailing).offset(12)
+            $0.trailing.equalTo(downloadButton.snp.leading).offset(-12)
             $0.centerY.equalToSuperview()
+        }
+        
+        titleStackView.snp.makeConstraints {
+            $0.height.lessThanOrEqualTo(iconImageView)
         }
         
         downloadButton.snp.makeConstraints {
@@ -111,19 +117,26 @@ class ChartTableViewCell: UITableViewCell {
         titleLabel.text = appData.title
         subtitleLabel.text = appData.subtitle
         
+        let price = appData.price
         switch appData.downloadState {
         case .installed:
-            downloadButton.configureButton(configType: .gray, title: "열기", fontSize: 17, fontWeight: .semibold, cornerStyle: .capsule)
+            downloadButton.configureButton(configType: .gray, title: "열기", fontSize: 16, fontWeight: .bold, cornerStyle: .capsule)
         case .download:
             if appData.price == 0 {
-                downloadButton.configureButton(configType: .gray, title: "받기", fontSize: 17, fontWeight: .semibold, cornerStyle: .capsule)
+                downloadButton.configureButton(configType: .gray, title: "받기", fontSize: 16, fontWeight: .bold, cornerStyle: .capsule)
             } else {
-                downloadButton.configureButton(configType: .gray, title: appData.price.toWonString(), fontSize: 17, fontWeight: .semibold, cornerStyle: .capsule)
+                let size: CGFloat = {
+                    if price < 10000 { return 16 }
+                    else if price < 100000 { return 14 }
+                    else { return 11 }
+                }()
+                
+                downloadButton.configureButton(configType: .gray, title: price.toWonString(), fontSize: size, fontWeight: .bold, cornerStyle: .capsule)
             }
         case .redownload:
-            downloadButton.configureButton(configType: .plain, systemName: "icloud.and.arrow.down", pointSize: 17, symbolWeight: .semibold)
+            downloadButton.configureButton(configType: .plain, systemName: "icloud.and.arrow.down", pointSize: 16, symbolWeight: .semibold)
         case .update:
-            downloadButton.configureButton(configType: .gray, title: "업데이트", fontSize: 17, fontWeight: .semibold, cornerStyle: .capsule)
+            downloadButton.configureButton(configType: .gray, title: "업데이트", fontSize: 16, fontWeight: .bold, cornerStyle: .capsule)
         }
     }
 }
