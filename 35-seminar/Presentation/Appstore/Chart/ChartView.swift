e9848cbf9d64f7935979ae9b750510ff1c802e46
Yurim Kim <yurim830@gmail.com>
2024-10-29 22:45:12 +0900

    [Feat] #8 - ChartView 뼈대 구성

--- 35-seminar/Presentation/Appstore/Chart/ChartView.swift
+++ 35-seminar/Presentation/Appstore/Chart/ChartView.swift
@@ -2,11 +2,43 @@
 //  ChartView.swift
 //  35-seminar
 //
-//  Created by 김유림 on 10/29/24.
+//  Created by 김유림 on 10/26/24.
 //
 
 import UIKit
 
 class ChartView: UIView {
     
+    // MARK: - Properties
+    let headerView = UITableViewHeaderFooterView()
+    let tableView = UITableView()
+    
+    // MARK: - Methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setHierarchy()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI
+    
+    private func setUI() {
+        tableView.backgroundColor = .yellow
+        
+    }
+    
+    private func setHierarchy() {
+        self.addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
