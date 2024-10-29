644a3764f1bb78ec4129fc5462146ebf4b7ef7f9
Yurim Kim <yurim830@gmail.com>
2024-10-30 01:00:59 +0900

    [Add] #8 - 둘러보기 VC 생성

---
+++ 35-seminar/Presentation/Appstore/Explore/MainExploreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainExploreViewController.swift
+//  35-seminar
+//
+//  Created by 김유림 on 10/30/24.
+//
+
+import UIKit
+
+class MainExploreViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
