b371ea34a22031fbdaca1af538c06f627b8878e9
Yurim Kim <yurim830@gmail.com>
2024-10-30 00:40:36 +0900

    [Add, Feat] #8 - (Category) enum 생성

---
+++ 35-seminar/Entity/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  35-seminar
+//
+//  Created by 김유림 on 10/30/24.
+//
+
+import Foundation
+
+enum Category: String {
+    case finance = "금융"
+    case socialNetwork = "소셜 네트워크"
+}
