8fb46137b461a54c30dcf7d9a99e44fd6568ab59
Yurim Kim <yurim830@gmail.com>
2024-11-06 04:02:21 +0900

    [Feat] #11 - 금융 둘러보기로 push하는 버튼 구현

--- 35-seminar/Presentation/Appstore/Explore/MainExploreViewController.swift
+++ 35-seminar/Presentation/Appstore/Explore/MainExploreViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 
 class MainExploreViewController: BaseViewController {
     
+    private let financeButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func setStyle() {
+        view.backgroundColor = .systemBackground
+        
+        financeButton.configureButton(configType: .filled, title: " 금융", fontWeight: .semibold, systemName: "dollarsign.circle", foregroundColor: .label, backgroundColor: .systemGray5)
+        
+        view.addSubview(financeButton)
+        
+        financeButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func setAddTarget() {
+        financeButton.addTarget(self, action: #selector(tappedFinanceButton), for: .touchUpInside)
+    }
+    
+    @objc func tappedFinanceButton() {
+        let categoryExploreVC = CategoryExploreViewController()
+        navigationController?.pushViewController(categoryExploreVC, animated: true)
     }
 }
