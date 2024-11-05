6d724d209bf51ac16f24ee0f6b74b24681cc5534
Yurim Kim <yurim830@gmail.com>
2024-11-06 03:49:01 +0900

    [Feat] #11 - 카테고리둘러보기뷰 2가지 셀 등록 및 구현

--- 35-seminar/Presentation/Appstore/Explore/CategoryExploreViewController.swift
+++ 35-seminar/Presentation/Appstore/Explore/CategoryExploreViewController.swift
@@ -8,8 +8,120 @@
 import UIKit
 
 class CategoryExploreViewController: BaseViewController {
+    // MARK: - Properties
+    private var bannerApps: [App] = []
+    private var essentialApps: [App] = []
+    private var paidApps: [App] = []
+    private var freeApps: [App] = []
+    
+    // MARK: - Methods
+    private let categoryExploreView = CategoryExploreView()
+    
+    override func loadView() {
+        view = categoryExploreView
+    }
     
     override func viewDidLoad() {
+        setListApps()
+        setBannerApps()
+        
         super.viewDidLoad()
+    }
+    
+    override func setDelegate() {
+        categoryExploreView.tableView.dataSource = self
+        categoryExploreView.tableView.delegate = self
+        categoryExploreView.tableView.register(ExploreListTableViewCell.self, forCellReuseIdentifier: ExploreListTableViewCell.identifier)
+        categoryExploreView.tableView.register(ExploreBannerTableViewCell.self, forCellReuseIdentifier: ExploreBannerTableViewCell.identifier)
+    }
+    
+    private func setListApps() {
+        let apps = App.sampleApps
+        
+        var essentialAppsCount = 0
+        var paidAppsCount = 0
+        var freeAppsCount = 0
+        
+        for app in apps {
+            if app.price == 0 {
+                guard freeAppsCount < 15 else { continue }
+                freeApps.append(app)
+                freeAppsCount += 1
+            } else {
+                guard paidAppsCount < 15 else { continue }
+                paidApps.append(app)
+                paidAppsCount += 1
+            }
+            
+            if essentialAppsCount > 15,
+               paidAppsCount >= 15,
+               freeAppsCount >= 15 { break }
+            
+            guard essentialAppsCount < 15 else { continue }
+            essentialApps.append(app)
+            essentialAppsCount += 1
+        }
+    }
+    
+    private func setBannerApps() {
+        bannerApps = Array(App.sampleApps[0..<4])
+    }
+}
+
+extension CategoryExploreViewController: UITableViewDataSource {
+    // section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    // cell
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.section {
+            // banner apps
+        case 0:
+            guard let cell = categoryExploreView.tableView.dequeueReusableCell(
+                withIdentifier: ExploreBannerTableViewCell.identifier,
+                for: indexPath
+            ) as? ExploreBannerTableViewCell else { return UITableViewCell() }
+            
+            cell.bind(apps: bannerApps)
+            return cell
+            
+            // list apps
+        default:
+            guard let cell = categoryExploreView.tableView.dequeueReusableCell(
+                withIdentifier: ExploreListTableViewCell.identifier,
+                for: indexPath
+            ) as? ExploreListTableViewCell else { return UITableViewCell() }
+            
+            switch indexPath.section {
+            case 1:
+                cell.bind(apps: essentialApps)
+            case 2:
+                cell.bind(apps: paidApps)
+            default:
+                cell.bind(apps: freeApps)
+            }
+            
+            return cell
+        }
+    }
+}
+
+
+extension CategoryExploreViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return CGFloat(ExploreBannerTableViewCell.height)
+            
+        default :
+            return CGFloat(ExploreListTableViewCell.height)
+        }
     }
 }
