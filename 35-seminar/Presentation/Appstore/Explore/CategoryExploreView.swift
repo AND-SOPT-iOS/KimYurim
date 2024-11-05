4d7347fa344133cca84be2d7184eb230d96aad49
Yurim Kim <yurim830@gmail.com>
2024-11-06 03:46:30 +0900

    [Feat] #11 - 둘러보기뷰 설정

--- 35-seminar/Presentation/Appstore/Explore/CategoryExploreView.swift
+++ 35-seminar/Presentation/Appstore/Explore/CategoryExploreView.swift
@@ -7,14 +7,35 @@
 
 import UIKit
 
-class CategoryExploreView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class CategoryExploreView: BaseView {
+    // MARK: - Properties
+    let tableView = UITableView()
+    
+    // MARK: - Methods
+    private var rowHeight: CGFloat?
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setDelegate() { }
+    
+    override func setUI() {
+//        tableView.rowHeight = CGFloat(ExploreListTableViewCell.height)
+    }
+    
+    override func setHierarchy() {
+        self.addSubview(tableView)
+    }
+    
+    override func setConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
