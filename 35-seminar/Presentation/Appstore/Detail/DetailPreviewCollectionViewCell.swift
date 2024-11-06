037de5488f27430487b88a9a3ef37d16d1ea994c
Yurim Kim <yurim830@gmail.com>
2024-11-06 09:07:38 +0900

    [Feat] #10 - 셀 구성 완료

--- 35-seminar/Presentation/Appstore/Detail/DetailPreviewCollectionViewCell.swift
+++ 35-seminar/Presentation/Appstore/Detail/DetailPreviewCollectionViewCell.swift
@@ -9,4 +9,43 @@ import UIKit
 
 class DetailPreviewCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    static let identifier = String(describing: DetailPreviewCollectionViewCell.self)
+    
+    private let imageView = UIImageView()
+    
+    // MARK: - Methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setHierarchy()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.backgroundColor = .systemGray5
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
+        imageView.layer.borderColor = UIColor.systemGray5.cgColor
+        imageView.layer.borderWidth = 1
+    }
+    
+    private func setHierarchy() {
+        contentView.addSubview(imageView)
+    }
+    
+    private func setConstraints() {
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func bind(image: UIImage?) {
+        imageView.image = image
+    }
 }
