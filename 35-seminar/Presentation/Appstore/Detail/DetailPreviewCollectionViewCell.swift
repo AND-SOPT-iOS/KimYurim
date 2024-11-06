c28697161dd49d544f92fe607460b5a393cc3c77
Yurim Kim <yurim830@gmail.com>
2024-11-06 09:06:41 +0900

    [Add] #10 - 미리보기 컬렉션뷰 셀 생성

---
+++ 35-seminar/Presentation/Appstore/Detail/DetailPreviewCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DetailPreviewCollectionViewCell.swift
+//  35-seminar
+//
+//  Created by 김유림 on 11/6/24.
+//
+
+import UIKit
+
+class DetailPreviewCollectionViewCell: UICollectionViewCell {
+    
+}
