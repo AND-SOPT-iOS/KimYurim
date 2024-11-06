e662a73a6ee68b97167ce71744c39f71b451bf36
Yurim Kim <yurim830@gmail.com>
2024-11-06 13:34:06 +0900

    [Add] #10 - (FeedbackCollectionViewCell) 생성

---
+++ 35-seminar/Presentation/Appstore/Detail/DetailFeedbackCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DetailFeedbackCollectionViewCell.swift
+//  35-seminar
+//
+//  Created by 김유림 on 11/6/24.
+//
+
+import UIKit
+
+class DetailFeedbackCollectionViewCell: UICollectionViewCell {
+    
+}
