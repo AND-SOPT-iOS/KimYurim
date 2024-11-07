6a5ad99e055a6da5f499bac7c57b42613f62b8e4
Yurim Kim <yurim830@gmail.com>
2024-11-08 04:54:55 +0900

    [Add] #17 - UserView, VC 추가

---
+++ 35-seminar/Presentation/Appstore/User/View/UserView.swift
@@ -0,0 +1,20 @@
+//
+//  UserView.swift
+//  35-seminar
+//
+//  Created by 김유림 on 11/8/24.
+//
+
+import UIKit
+
+class UserView: BaseView {
+    override func setDelegate() { }
+    
+    override func setUI() {
+        backgroundColor = .systemGray5
+    }
+    
+    override func setHierarchy() { }
+    
+    override func setConstraints() { }
+}
---
+++ 35-seminar/Presentation/Appstore/User/ViewController/UserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserViewController.swift
+//  35-seminar
+//
+//  Created by 김유림 on 11/8/24.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
