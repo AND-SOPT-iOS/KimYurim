b231ff230d3bb489b27dd39ef73f5b5222e1a04b
Yurim Kim <yurim830@gmail.com>
2024-11-08 14:41:18 +0900

    [Feat] #17 - hobby 조회 구현

--- 35-seminar/Presentation/Appstore/User/ViewController/UserViewController.swift
+++ 35-seminar/Presentation/Appstore/User/ViewController/UserViewController.swift
@@ -19,12 +19,38 @@ class UserViewController: BaseViewController {
         super.viewDidLoad()
     }
     
+    override func setNavigationBar() {
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18, weight: .semibold)]
+        self.navigationItem.title = "계정"
+    }
+    
     override func setDelegate() { }
     
     override func setStyle() { }
     
     override func setAddTarget() { }
     
-    override func bind() { }
+    override func bind() {
+        let username = "yurim"
+        let password = "abc"
+        
+        LoginService.shared.login(username: username, password: password) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let token):
+                MyHobbyService.shared.getHobby(token: token) { [weak self] result in
+                    guard let self = self else { return }
+                    switch result {
+                    case .success(let hobby):
+                        userView.bind(user: RegisterRequestDTO(username: username, password: password, hobby: hobby))
+                    case .failure(let error):
+                        print("hobby 조회 에러: \(error.errorMessage)")
+                    }
+                }
+            case .failure(let error):
+                print("token 조회 에러: \(error.errorMessage)")
+            }
+        }
+    }
 }
 
