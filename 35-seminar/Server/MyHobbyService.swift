a348e8a73315fde57fa72b09617db73a7f055c47
Yurim Kim <yurim830@gmail.com>
2024-11-08 14:42:22 +0900

    [Add, Feat] #17 - 취미 조회 함수 구현

--- 35-seminar/Server/MyHobbyService.swift
+++ 35-seminar/Server/MyHobbyService.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import Alamofire
+
+class MyHobbyService {
+    static let shared = MyHobbyService()
+    init() { }
+    
+    func getHobby(token: String,
+               completion: @escaping (Result<String, NetworkError>) -> Void) {
+        
+        let url = Environment.baseURL + "/user/my-hobby"
+        
+        let headers: HTTPHeaders = [
+            "token": token
+        ]
+        
+        AF.request(url,
+                   method: .get,
+                   headers: headers)
+        .validate()
+        .responseDecodable(of: MyHobbyResponse.self) { [weak self] response in
+            guard let statusCode = response.response?.statusCode,
+                  let data = response.data,
+                  let _ = self else {
+                completion(.failure(.unknownError))
+                return
+            }
+            
+            switch response.result {
+            case .success(let loginResponse):
+                completion(.success(loginResponse.result.hobby))
+            case .failure:
+                let error = Network.shared.handleStatusCode(statusCode, data: data)
+                completion(.failure(error))
+            }
+        }
+    }
+}
