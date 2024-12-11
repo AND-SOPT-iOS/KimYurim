62063cbaa3078eb3505690629060454f3e038f01
Yurim Kim <yurim830@gmail.com>
2024-12-11 20:19:57 +0900

    [Add] #26 - AppDetailView 생성

---
+++ AppStore-SwiftUI/SeminarSwiftUI/AppStore/View/AppDetail/AppDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AppDetailView.swift
+//  SeminarSwiftUI
+//
+//  Created by 김유림 on 12/11/24.
+//
+
+import SwiftUI
+
+struct AppDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppDetailView()
+}
