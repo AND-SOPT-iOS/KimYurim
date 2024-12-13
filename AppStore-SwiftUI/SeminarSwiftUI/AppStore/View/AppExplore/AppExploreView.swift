f330d7e9f514c7be112b629cc49edaaf8d1318d1
Yurim Kim <yurim830@gmail.com>
2024-12-13 20:39:52 +0900

    [Style] #26 - Explore 뷰 완성

--- AppStore-SwiftUI/SeminarSwiftUI/AppStore/View/AppExplore/AppExploreView.swift
+++ AppStore-SwiftUI/SeminarSwiftUI/AppStore/View/AppExplore/AppExploreView.swift
@@ -11,12 +11,25 @@ struct AppExploreView: View {
     @StateObject private var viewModel = AppExploreViewModel()
     
     var body: some View {
-        BannerHStack(bannerApps: viewModel.bannerApps)
+        ScrollView {
+            VStack(spacing: 40) {
+                BannerHSlideView(bannerApps: viewModel.bannerApps)
+                
+                AppListHSlideView(sectionTitle: "필수 금융 앱", apps: viewModel.essentialApps)
+                
+                AppListHSlideView(sectionTitle: "유료 순위", apps: viewModel.paidApps)
+                
+                AppListHSlideView(sectionTitle: "무료 순위", apps: viewModel.freeApps)
+            }
+        }
+        .padding(.horizontal, 20)
     }
 }
 
+
 // MARK: - Components
-struct BannerHStack: View {
+
+struct BannerHSlideView: View {
     let bannerApps: [AppData]
     let width = UIScreen.main.bounds.width
     
@@ -24,23 +37,46 @@ struct BannerHStack: View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(bannerApps, id: \.id) { app in
-                    ZStack(alignment: .bottom) {
-                        app.bannerImage
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .background(Color.gray)
+                    VStack(alignment: .leading) {
+                        BannerBigTitleView(app: app)
                         
-                        BannerTitleView(app: app)
+                        ZStack(alignment: .bottom) {
+                            app.bannerImage
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .background(Color.gray)
+                            
+                            BannerMiniTitleView(app: app)
+                        }
+                        .frame(width: width - 40, height: width / 2)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                     }
-                    .frame(width: width - 20, height: width / 2)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
             }
         }
     }
 }
 
-struct BannerTitleView: View {
+struct BannerBigTitleView: View {
+    let app: AppData
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(app.tag ?? "앱")
+                .font(Font.system(size: 12, weight: .medium))
+                .foregroundStyle(.blue)
+            
+            Text(app.title)
+                .font(Font.system(size: 22, weight: .medium))
+            
+            Text(app.subtitle)
+                .font(Font.system(size: 19, weight: .regular))
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
+struct BannerMiniTitleView: View {
     let app: AppData
     
     var body: some View {
@@ -48,7 +84,7 @@ struct BannerTitleView: View {
             iconImage
             titleVStack
             Spacer()
-            DownloadButton(state: app.downloadState, buttonColor: .gray) { }
+            DownloadButton(state: app.downloadState, buttonColor: .white) { }
         }
         .padding(20)
         .frame(maxWidth: .infinity)
@@ -58,22 +94,60 @@ struct BannerTitleView: View {
     var iconImage: some View {
         app.iconImage
             .resizable()
-            .aspectRatio(contentMode: .fit)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .aspectRatio(contentMode: .fill)
             .frame(width: 50, height: 50)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
     
     var titleVStack: some View {
         VStack(alignment: .leading) {
             Text(app.title)
                 .font(Font.system(size: 17, weight: .semibold))
+                .lineLimit(1)
             Text(app.subtitle)
                 .font(Font.system(size: 15, weight: .regular))
+                .lineLimit(1)
         }
         .foregroundStyle(.white)
     }
 }
 
+struct AppListHSlideView: View {
+    let sectionTitle: String
+    let apps: [AppData]
+    let width = UIScreen.main.bounds.width
+    let rows = [
+        GridItem(.flexible(), spacing: 20),
+        GridItem(.flexible(), spacing: 20),
+        GridItem(.flexible(), spacing: 20)
+    ]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            sectionTitleView
+            
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: rows, spacing: 20) {
+                    ForEach(apps, id: \.id) { app in
+                        AppCell(app: app)
+                            .frame(width: width - 40)
+                    }
+                }
+            }
+        }
+    }
+    
+    var sectionTitleView: some View {
+        HStack {
+            Text(sectionTitle)
+                .font(Font.system(size: 24, weight: .bold))
+            
+            Image(systemName: "chevron.right")
+                .font(Font.system(size: 20, weight: .bold))
+                .foregroundStyle(.gray)
+        }
+    }
+}
 
 #Preview {
     AppExploreView()
